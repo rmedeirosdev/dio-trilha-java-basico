0f3ff017a536359f93525ba762e7d98e047fc0e4
Priv <raphaelmedeiros2004@gmail.com>
2024-06-18 16:25:18 -0300

    Adicionando outros modulos relacionados a estrutura de repeticao

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+           contador ++;
+        }
+        System.out.println("Contamos "+ contador + " Carneirinhos");
+    }
+}
