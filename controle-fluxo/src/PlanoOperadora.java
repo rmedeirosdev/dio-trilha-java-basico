b21c54c696634c97e913ae80f84e8abee300e3eb
Priv <raphaelmedeiros2004@gmail.com>
2024-06-18 12:47:47 -0300

    Adicionando a pasta controle de fluxo e exemplo de switch case

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,21 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+
+        String plano = "Turbo";
+
+        switch (plano) {
+            case "Basic":
+                System.out.println("100 minutos de ligação");
+                break;
+            case "Midia":
+                System.out.println("WhatsApp e Instagram grátis");
+                break;
+            case "Turbo":
+                System.out.println("5gb Youtube");
+                break;
+        
+            default: System.out.println("Nenhum plano selecionado para esse operadora!");
+                break;
+        }
+    }
+}
